3079e30ca8a8d24df19c4451216de651b4e9ed0d
Walker-00 <85013114+Walker-00@users.noreply.github.com>
2022-03-02 11:19:38 +0630

    Add files via upload

---
+++ Wayload/__init__.py
@@ -0,0 +1,6 @@
+from payload.hide import hd
+from payload.extract import apk
+from payload.extract import png
+from payload.extract import jpg
+from payload.extract import jpeg
+from payload.extract import gif
